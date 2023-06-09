b8c7cfde74be95e4348f3619986cee3982d76ca4
Harith Sami <66371106+HarithSami@users.noreply.github.com>
2023-06-09 21:21:41 +0100

    included resume view

--- main.py
+++ main.py
@@ -33,14 +33,54 @@ def user_facing():
         page_icon="📋",
     )
 
+def get_resume():
+    with open('assets/resume-template-2.html', 'r') as file:
+        html_string = file.read()
+    return html_string
+
+def get_coverletter():
+    with open('assets/coverletter-template.html', 'r') as file:
+        html_string = file.read()
+    return html_string
+
+def get_questions():
+    with open('assets/interviewquestions-template.html', 'r') as file:
+        html_string = file.read()
+    return html_string
 
 #this is suppose to be the main function, but the variables are not being passed
 st.header("Smart Apply 📋")
+page_icon="📋"
+st.subheader("Generated Resume!")
+
+toggle_button1 = st.checkbox("Hide Resume")
+st.empty()
+if not toggle_button1:
+    #Displays the resume
+    st.markdown(get_resume(), unsafe_allow_html=True)
+
+toggle_button2 = st.checkbox("Hide Cover Letter")
+st.empty()
+if not toggle_button2:
+    #Displays the resume
+    st.markdown(get_coverletter(), unsafe_allow_html=True)
+
+toggle_button3 = st.checkbox("Hide Interview Questions")
+st.empty()
+if not toggle_button3:
+    #Displays the resume
+    st.markdown(get_questions(), unsafe_allow_html=True)
+
+
+
 with st.sidebar:
     st.title("Place your information:")
     input_linkedin = st.text_input("Place Your Linkedin URL")
     input_position = st.text_input("Place the job posting page")
     github_linkedin = st.text_input("Place Your Github URL")
+    existing_resume = st.file_uploader("Upload your existing resume")
+
+
 
 
     st.write("what do you want to generate?")
